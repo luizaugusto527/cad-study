3bd586ed5ffaa716edb2fab7ebe17d172406d2c0
unknown <luizaugustotpires@gmail.com>
2021-11-13 20:36:55 -0300

    banner

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { BannerComponent } from './banner/banner.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent
+    NavBarComponent,
+    BannerComponent
   ],
   imports: [
     BrowserModule
