cf9cce3d190443dece54526e72b5a9cef0bac92d
unknown <luizaugustotpires@gmail.com>
2021-11-14 16:48:07 -0300

    tables

--- src/app/banner/banner.component.ts
+++ src/app/banner/banner.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Student } from './student';
+
 
 @Component({
  
@@ -7,32 +7,7 @@ import { Student } from './student';
   styleUrls: ['./banner.component.css']
 })
 export class BannerComponent implements OnInit {
-  students:Student[] = [{
-    nome:"Luiz",
-    idade:27,
-    curso:"Análise e desenvolvimento de sistemas",
-    semestre:"3°",
-    cidade:"Sorocaba"
 
-  },
-  {
-    nome:"Maria",
-    idade:18,
-    curso:"Psicologia",
-    semestre:"5°",
-    cidade:"Itu"
-
-  },
-  {
-    nome:"Rodrigo",
-    idade:23,
-    curso:"Engenharia Elétrica",
-    semestre:"4°",
-    cidade:"Sorocaba"
-
-  }
-
-]
   constructor() { }
 
   ngOnInit(): void {
