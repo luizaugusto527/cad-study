f8afa148a55abf3895aae4b44aa7db31814c704e
unknown <luizaugustotpires@gmail.com>
2021-12-06 23:23:39 -0300

    http put

--- src/app/students/students-list.service.ts
+++ src/app/students/students-list.service.ts
@@ -25,7 +25,10 @@ export class StudentsListService {
      
   }
 
- 
+ updateStudentList(student:Student):Observable<Student>{
+  const updateUrl = `http://localhost:3000/students/${student.id}`
+  return this.http.put<Student>(updateUrl,student,this.httpOptions)
+ }
 
 
 }
--- src/app/students/students.component.ts
+++ src/app/students/students.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Student } from './student';
 import { StudentsListService } from './students-list.service';
-import { FormGroup,FormBuilder,Validators, EmailValidator } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-students',
@@ -9,53 +10,66 @@ import { FormGroup,FormBuilder,Validators, EmailValidator } from '@angular/forms
   styleUrls: ['./students.component.css']
 })
 export class StudentsComponent implements OnInit {
-  students:Student[] =[]
-  isEmpty:boolean = false
-  studentForm:FormGroup;
-    mostra:Student = {
-      id:0,
-      nome:"null",
-      idade:0,
-      semestre:"0",
-      curso:"",
-      email:"",
-      cidade:''
-    }
-  constructor(public student:StudentsListService, private formBuilder:FormBuilder) {
+  students: Student[] = []
+  isEmpty: boolean = false
+  studentForm: FormGroup;
+  mostra: Student = {
+    id: 0,
+    nome: "null",
+    idade: 0,
+    semestre: "0",
+    curso: "",
+    email: "",
+    cidade: ''
+  }
+  constructor(public student: StudentsListService, private formBuilder: FormBuilder) {
     this.studentForm = this.formBuilder.group({
-      nome:['',Validators.required],
-      idade:['',Validators.required],
-      email:['',[Validators.required,Validators.email]],
-      curso:['',Validators.required],
-      semestre:['',Validators.required],
-      cidade:['',Validators.required]
+      id: [''],
+      nome: ['', Validators.required],
+      idade: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      curso: ['', Validators.required],
+      semestre: ['', Validators.required],
+      cidade: ['', Validators.required]
     })
   }
 
   ngOnInit(): void {
     this.student.getstudentsList().subscribe({
-      next:studentList =>{
+      next: studentList => {
         this.students = studentList
         if (this.students.length == 0) {
           this.isEmpty = true
         }
       }
     })
-       
+
   }
 
-  edit(id:number){
-   this.mostra =  this.students.find(name => name.id == id)!; 
-   this.studentForm.setValue({
-    nome: this.mostra.nome, 
-    email: this.mostra.email, 
-    curso: this.mostra.curso, 
-    semestre: this.mostra.semestre, 
-    idade: this.mostra.idade, 
-    cidade: this.mostra.cidade, 
-  
-  });
-    
+  edit(id: number) {
+    this.mostra = this.students.find(name => name.id == id)!;
+    this.studentForm.setValue({
+      id: this.mostra.id,
+      nome: this.mostra.nome,
+      email: this.mostra.email,
+      curso: this.mostra.curso,
+      semestre: this.mostra.semestre,
+      idade: this.mostra.idade,
+      cidade: this.mostra.cidade,
+
+    });
+
   }
 
+  updateStudent() {
+    const editedStudent = this.studentForm.value
+    this.student.updateStudentList(editedStudent).subscribe(() => {
+      console.log("Atualizado com sucesso");
+      window.location.reload();
+
+    }
+
+    )
+
+  }
 }
