699e38ed4c83070b37c91baf2cfaeaf94a4e7987
unknown <luizaugustotpires@gmail.com>
2021-12-06 21:46:28 -0300

    edit frontend

--- src/app/students/students.component.ts
+++ src/app/students/students.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Student } from './student';
 import { StudentsListService } from './students-list.service';
+import { FormGroup,FormBuilder,Validators, EmailValidator } from '@angular/forms';
 
 @Component({
   selector: 'app-students',
@@ -8,9 +9,29 @@ import { StudentsListService } from './students-list.service';
   styleUrls: ['./students.component.css']
 })
 export class StudentsComponent implements OnInit {
-  constructor(public student:StudentsListService) { }
   students:Student[] =[]
   isEmpty:boolean = false
+  studentForm:FormGroup;
+    mostra:Student = {
+      id:0,
+      nome:"null",
+      idade:0,
+      semestre:"0",
+      curso:"",
+      email:"",
+      cidade:''
+    }
+  constructor(public student:StudentsListService, private formBuilder:FormBuilder) {
+    this.studentForm = this.formBuilder.group({
+      nome:['',Validators.required],
+      idade:['',Validators.required],
+      email:['',[Validators.required,Validators.email]],
+      curso:['',Validators.required],
+      semestre:['',Validators.required],
+      cidade:['',Validators.required]
+    })
+  }
+
   ngOnInit(): void {
     this.student.getstudentsList().subscribe({
       next:studentList =>{
@@ -21,6 +42,20 @@ export class StudentsComponent implements OnInit {
       }
     })
        
+  }
+
+  edit(id:number){
+   this.mostra =  this.students.find(name => name.id == id)!; 
+   this.studentForm.setValue({
+    nome: this.mostra.nome, 
+    email: this.mostra.email, 
+    curso: this.mostra.curso, 
+    semestre: this.mostra.semestre, 
+    idade: this.mostra.idade, 
+    cidade: this.mostra.cidade, 
+  
+  });
+    
   }
 
 }
